debad5588ac041b5188273b156bcbfe82c058a9f
kup1o <kup1o@pm.me>
2024-02-05 15:22:04 +0300

    feat: tests for system_info entries

---
+++ tests/unit_tests.rs
@@ -0,0 +1,29 @@
+#[cfg(test)]
+mod tests {
+    use system_info::*;
+
+    #[test]
+    fn test_get_cpu_info() {
+        let result = get_cpu_info();
+        assert!(result.starts_with("CPU:"));
+    }
+
+    #[test]
+    fn test_get_gpu_info() {
+        let result = get_gpu_info();
+        assert!(result.starts_with("GPU:"));
+    }
+
+    #[test]
+    fn test_get_display_info() {
+        let result = get_display_info();
+        println!("{}", result);
+        assert!(result.starts_with("Display:"));
+    }
+
+    #[test]
+    fn test_get_ram_info() {
+        let result = get_ram_info();
+        assert!(result.starts_with("RAM:"));
+    }
+}
