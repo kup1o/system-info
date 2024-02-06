b4b64eba6cdff9c96428ccffef49cc4055a0e9ce
kup1o <kup1o@pm.me>
2024-02-06 07:28:26 +0300

    chore: cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 mod system_info;
 
-pub use system_info::{get_cpu_info, get_gpu_info, get_display_info, get_ram_info};
+pub use system_info::{get_cpu_info, get_display_info, get_gpu_info, get_ram_info};
